7f8c8eb9fa3e489cd254906e1d9e11d02b2866da
John Denver <johndenverbidong1@gmail.com>
2022-12-08 17:59:06 +0800

    change h1 to h2

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -60,9 +60,9 @@ function Country() {
           />
         </div>
         <div>
-          <h1 className="text-3xl font-bold mb-10 dark:text-white">
+          <h2 className="text-3xl font-bold mb-10 dark:text-white">
             {country.name}
-          </h1>
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-16">
             <div className="flex flex-col gap-3">
               <div className="flex gap-3">
